cca0461593910ae8d7cd19de03e1dbb54f053e73
durieux mickael <mdurieux@prune>
2014-10-23 16:00:23 +0200

    Deuxième test

--- test/test.js
+++ test/test.js
@@ -7,3 +7,8 @@ PalettoTestCase.prototype.testStory1 = function () {
     p.init();
     assertTrue(p.checkJuxtaposition());
 };
+
+var PalettoTestCase2 = TestCase("PalettoTestCase2");
+PalettoTestCase2.prototype.testStory2 = function () {
+    assertTrue(p.chooseMarble(5,0) == "yellow");
+};
