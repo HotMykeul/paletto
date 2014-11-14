a39a1e607d18fc9bee3a6bbb13820a703753ca6c
durieux mickael <mdurieux@prune>
2014-11-14 15:00:21 +0100

    Quatrieme test

--- test/test.js
+++ test/test.js
@@ -20,3 +20,15 @@ PalettoTestCase3.prototype.testStory3 = function () {
     assertTrue(p.count_marble() == 35);
     assertTrue(p.check_marble_nb(0) == 1);
 };
+
+var PalettoTestCase4 = TestCase("PalettoTestCase4");
+PalettoTestCase4.prototype.testStory4 = function () {
+    p.change_player();
+    p.get_marble(0, 'a');
+    p.get_marble(5, 'f');
+    assertTrue(p.check_marble(0, 'a') == "null");
+    assertTrue(p.check_marble(5, 'f') == "null");
+    assertTrue(p.count_marble() == 33);
+    assertTrue(p.check_marble_nb(1) == 2);
+
+};
